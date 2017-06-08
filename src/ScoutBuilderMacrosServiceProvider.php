65905cafe97e55fc510923442c7c3f5c801c6c08
Julien Bourdeau <julien@sigerr.org>
2017-06-08 16:35:31 +0200

    Check if you are using Algolia

--- src/ScoutBuilderMacrosServiceProvider.php
+++ src/ScoutBuilderMacrosServiceProvider.php
@@ -9,6 +9,10 @@ class ScoutBuilderMacrosServiceProvider extends ServiceProvider
 {
     public function boot()
     {
+        if (!class_exists('AlgoliaSearch\Client')) {
+            throw new Exception("It seems like you are not using Algolia. This package requires the Algolia engine.", 1);
+        }
+
         require_once __DIR__.'/macros.php';
     }
 }
