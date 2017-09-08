ac6380ec08cd58ff01f79104335335e441bbb5b5
Julien Bourdeau <julien@sigerr.org>
2017-09-08 18:06:49 +0200

    feat(ua): Add custom UA suffix

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -9,6 +9,8 @@ class ServiceProvider extends LaravelServiceProvider
 {
     public function boot()
     {
+        AlgoliaUserAgent::addSuffixUserAgentSegment('; Laravel Scout macros package', '0.x');
+
         if (!class_exists('AlgoliaSearch\Client')) {
             throw new Exception("It seems like you are not using Algolia. This package requires the Algolia engine.", 1);
         }
