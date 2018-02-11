e046b91b0980b5356c2d9bb45673eb3f71759b6d
Gercil <gerciljunio@gmail.com>
2018-02-11 15:11:34 -0200

    laravel 5.6
    Signed-off-by: Gercil <gerciljunio@gmail.com>

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -3,13 +3,13 @@
 namespace Algolia\ScoutMacros;
 
 use Illuminate\Support\ServiceProvider as LaravelServiceProvider;
-
+use AlgoliaSearch\Version as AlgoliaUserAgent;
 
 class ServiceProvider extends LaravelServiceProvider
 {
     public function boot()
     {
-        // AlgoliaUserAgent::addSuffixUserAgentSegment('; Laravel Scout macros package', '0.x');
+        AlgoliaUserAgent::addSuffixUserAgentSegment('; Laravel Scout macros package', '0.x');
 
         if (!class_exists('AlgoliaSearch\Client')) {
             throw new Exception("It seems like you are not using Algolia. This package requires the Algolia engine.", 1);
